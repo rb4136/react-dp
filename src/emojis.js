aa28dd6f4363d57d571d8f95445f72e552900231
Richard Brower <richardtia@gmail.com>
2020-11-23 12:34:49 -0500

    Remove last emoji due to Chrome error

--- src/emojis.js
+++ src/emojis.js
@@ -1,34 +1,39 @@
 const emojis = [
-{
-  name: "Slightly Smiling Face",
-  description: "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not.",
-  emoji: "🙂"
-},
-{
-  name: "Rolling on the Floor Laughing",
-  description: "A yellow face with a big grin and scrunched, X-shaped eyes, tilted on its side as if rolling on the floor laughing (the internet acronym ROFL). Sheds two tears and tilts right on most platforms. Often conveys hysterical laughter more intense than Face With Tears of Joy.",
-  emoji: "🤣"
-},
-{
-  name: "Winking Face with Tongue",
-  description: " yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking.",
-  emoji: "😜"
-},
-{
-  name: "Face Vomiting",
-  description: "A yellow face with scrunched, X-shaped eyes spewing bright-green vomit. May represent physical illness or disgust, more intensely so than Nauseated Face.",
-  emoji: "🤮"
-},
-{
-  name: "Upside-Down Face",
-  description: "A classic smiley, turned upside down. Implemented as a flipped version of Slightly Smiling Face on most platforms. Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of Person Shrugging or the shruggie emoticon, ¯\_(ツ)_/¯.",
-  emoji: "🙃"
-},
-{
-  name: "Smiling Face with Hearts",
-  description: "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform.",
-  emoji: "🥰"
-}
-]
+  {
+    name: "Slightly Smiling Face",
+    description:
+      "A yellow face with simple, open eyes and a thin, closed smile. Conveys a wide range of positive, happy, and friendly sentiments. Its tone can also be patronizing, passive-aggressive, or ironic, as if saying This is fine when it’s really not.",
+    emoji: "🙂",
+  },
+  {
+    name: "Rolling on the Floor Laughing",
+    description:
+      "A yellow face with a big grin and scrunched, X-shaped eyes, tilted on its side as if rolling on the floor laughing (the internet acronym ROFL). Sheds two tears and tilts right on most platforms. Often conveys hysterical laughter more intense than Face With Tears of Joy.",
+    emoji: "🤣",
+  },
+  {
+    name: "Winking Face with Tongue",
+    description:
+      " yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking.",
+    emoji: "😜",
+  },
+  {
+    name: "Face Vomiting",
+    description:
+      "A yellow face with scrunched, X-shaped eyes spewing bright-green vomit. May represent physical illness or disgust, more intensely so than Nauseated Face.",
+    emoji: "🤮",
+  },
+  {
+    name: "Upside-Down Face",
+    description:
+      "A classic smiley, turned upside down. Implemented as a flipped version of Slightly Smiling Face on most platforms. Commonly used to convey irony, sarcasm, joking, or a sense of goofiness or silliness. Its intent can be similar to the bemused Oh well! of Person Shrugging or the shruggie emoticon, ¯_(ツ)_/¯.",
+    emoji: "🙃",
+    // },
+    // {
+    //   name: "Smiling Face with Hearts",
+    //   description: "A yellow face with smiling eyes, a closed smile, rosy cheeks, and several hearts floating around its head. Expresses a range of happy, affectionate feelings, especially being in love. The number of hearts (three or more) varies by platform.",
+    //   emoji: "🥰"
+  },
+];
 
 export default emojis;
